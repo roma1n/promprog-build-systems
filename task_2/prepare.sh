0db0dc3e563aaf6a5628053ec9d79ac80e166212
roma1n <golubtsowroman@gmail.com>
2021-04-08 23:57:48 +0300

    task_2

---
+++ task_2/prepare.sh
@@ -0,0 +1,14 @@
+apt install build-essential -y
+apt install cmake -y
+apt install libboost-all-dev -y
+apt install libssl-dev -y
+apt install sendmail -y
+apt install gsasl -y
+apt install libgsasl7-dev libgsasl7 -y
+apt install gnutls-bin -y
+apt install libgnutls28-dev -y
+apt install doxygen -y
+apt install graphviz -y
+apt install curl -y
+apt install libcurl4-openssl-dev -y
+apt update --fix-missing
